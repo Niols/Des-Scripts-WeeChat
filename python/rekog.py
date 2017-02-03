bb75e582ec5dab035a9c30d2d8372d8ab6ae3026
Niols <niols@niols.fr>
2017-02-03 14:41:11 +0100

    rekog: dirty fix for the case where the histograms don t have the same size

--- python/rekog.py
+++ python/rekog.py
@@ -32,16 +32,26 @@ from sys import version_info
 
 if version_info.major == 2:
     from urllib2 import urlopen
+    from cStringIO import StringIO as BytesIO
 else:
     from urllib.request import urlopen
-        
-from PIL import Image
+    from io import BytesIO
+
+try:
+    from PIL import Image
+except Exception: 
+    weechat.prnt('', 'This script requires PIL in order to work.')
+    weechat.prnt('', 'Get it now at: <http://python-pillow.org/>.')
+    
 from re import compile as compile_regex
+from re import IGNORECASE
 from os.path import join as join_path
+from math import sqrt
+from traceback import format_exc
 
 SCRIPT_NAME    = 'rekog'
 SCRIPT_AUTHOR  = 'Niols <niols@niols.fr>'
-SCRIPT_VERSION = '0.1'
+SCRIPT_VERSION = '0.2'
 SCRIPT_LICENSE = 'GPL'
 SCRIPT_DESC    = 'Recognizes URLs in incoming messages, compare the image they point to to a database, and replace them by an other string if needed.'
 
@@ -51,7 +61,7 @@ SCRIPT_DESC    = 'Recognizes URLs in incoming messages, compare the image they p
 def get_full_path(path):
     return join_path(weechat.info_get('weechat_dir', ''), SCRIPT_NAME, path)
 
-# =============================== [ URL regex ] ============================== #
+# ============================== [ URL regex ] =============================== #
 
 url_octet = r'(?:2(?:[0-4]\d|5[0-5])|1\d\d|\d{1,2})'
 url_ip_addr = r'%s(?:\.%s){3}' % (url_octet, url_octet)
@@ -59,7 +69,7 @@ url_label = r'[0-9a-z][-0-9a-z]*[0-9a-z]?'
 url_domain = r'%s(?:\.%s)*\.[a-z][-0-9a-z]*[a-z]?' % (url_label, url_label)
 url_regex = compile_regex(
     r'(http[s]?://(?:%s|%s)(?::\d+)?(?:/[^\])>\s]*)?)' % (url_domain, url_ip_addr),
-    re.I
+    IGNORECASE
 )
 
 # ========================== [ images comparison ] =========================== #
@@ -89,19 +99,24 @@ def fast_compare (im1, im2, threshold = 0):
     except IndexError:
         return False
 
-    diff_squares = [(h1[i] - h2[i]) ** 2 for i in range(len(h1))]
-    rms = math.sqrt(sum(diff_squares) / len(h1))
+    diff_squares = [(h1[i] - h2[i]) ** 2 for i in range(min(len(h1),len(h2)))]
+    rms = sqrt(sum(diff_squares) / len(h1))
 
+    weechat.prnt('', '%s: Matched with a RMS of %s.' % (SCRIPT_NAME, str(rms)))
+    
     return (rms <= threshold)
 
 # ============================= [ replacement ] ============================== #
 
 replacement_database = [
-    ('facebook_thumbsup', 'facebook_thumbsup.png', ':thumbsup:', 0)
+    ('facebook_thumbsup', 'facebook_thumbsup.png', ':thumbsup:', 10)
 ]
 
 def find_replacement (url):
-    im = Image.open(BytesIO(urlopen(url).read()))
+    try:
+        im = Image.open(BytesIO(urlopen(url).read()))
+    except IOError:
+        return None
 
     for (name, path, replacement, threshold) in replacement_database:
         if fast_compare (Image.open(get_full_path(path)), im, threshold):
@@ -112,14 +127,19 @@ def find_replacement (url):
 # ============================== [ callbacks ] =============================== #
 
 def cb_in_privmsg(data, modifier, modifier_data, message):
+    # This is the callback called at each incoming PRIVMSG. It must
+    # return a valid IRC command (and, in our case, a valid PRIVMSG).
+    
     try:
         for url in url_regex.findall(message):
             replacement = find_replacement(url)
             if replacement:
                 message = message.replace(url, replacement)
                 
-    except Exception as e:
-        weechat.prnt('', '%s: unexpected exception: %s' % (SCRIPT_NAME, str(e)))
+    except:
+        for line in format_exc().split('\n'):
+            if line:
+                weechat.prnt('', '%s: %s' % (SCRIPT_NAME, line))
         
     return message
 
@@ -132,4 +152,4 @@ if __name__ == '__main__':
                         SCRIPT_LICENSE, SCRIPT_DESC, '', ''):
 
         # If that works, we add our hook for incoming messages
-        weechat.hook_modifier('cb_in_privmsg', 'in_privmsg', '')
+        weechat.hook_modifier('irc_in_privmsg', 'cb_in_privmsg', '')
