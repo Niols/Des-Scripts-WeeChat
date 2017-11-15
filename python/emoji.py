53a4281ea6fd69e790a47de3b54f143cfa47672a
Niols <niols@niols.fr>
2017-11-15 02:53:52 +0100

    support sending of emojis

--- python/emoji.py
+++ python/emoji.py
@@ -3,7 +3,7 @@
 
 SCRIPT_NAME    = 'emoji'
 SCRIPT_AUTHOR  = 'Niols <niols@niols.fr>'
-SCRIPT_VERSION = '0.1'
+SCRIPT_VERSION = '0.2'
 SCRIPT_LICENSE = 'WTFPL'
 SCRIPT_DESC    = 'FIXME'
 
@@ -36,7 +36,10 @@ def get_name_from_cache(emoji):
         return None
 
 def get_emoji_from_cache(name):
-    return None #FIXME
+    for emoji in cache:
+        if cache[emoji] == name:
+            return emoji
+    return None
 
 def emoji_to_name(emoji):
     name = get_name_from_cache(emoji)
@@ -53,19 +56,65 @@ def emoji_to_name(emoji):
     add_to_cache(emoji, name)
     return name
 
+def emoji_from_name(name):
+    if name == '':
+        return None
+    for char in name:
+        if char not in 'abcdefghijklmnopqrstuvwxyz-':
+            return None
+    emoji = get_emoji_from_cache(name)
+    if emoji != None:
+        return emoji
+    r = requests.get (
+        'https://emojipedia.org/%s/' % name,
+        headers = headers
+    )
+    if r.status_code == 404:
+        return None
+    title_start = r.text.index('<title>') + 7
+    emoji = r.text[title_start]
+    return emoji
+
+def emoji_to_name_in_string(string):
+    string = string.decode('utf-8')
+    output = ''
+    for char in string:
+        output += ':'+emoji_to_name(char)+':' if ord(char) > 10000 else char.encode('utf-8')
+    return output
+
+def emoji_from_name_in_string(string):
+    string = string.split(':')
+    output = ''
+    prev_colon = False
+    for part in string:
+        if prev_colon:
+            new_part = emoji_from_name(part)
+            if new_part != None:
+                output += new_part.encode('utf-8')
+                prev_colon = False
+            else:
+                output += ':'+part
+                prev_colon = True
+        else:
+            output += part
+            prev_colon = True
+    return output
+
 ################################ [ Callbacks ] #################################
 
 def irc_in_privmsg(data, modifier, modifier_data, message):
-
     [before, content] = message.split(' :', 1)
-    content = content.decode('utf-8')
-    new_content = ''
+    content = emoji_to_name_in_string(content)
+    return before + ' :' + content
 
-    for char in content:
-        new_content += ':'+emoji_to_name(char)+':' if ord(char) > 10000 else char.encode('utf-8')
+def irc_out1_privmsg(data, modifier, modifier_data, message):
+    [before, content] = message.split(' :', 1)
+    content = emoji_from_name_in_string(content)
+    return before + ' :' + content
 
-    new_message = before + ' :' + new_content
-    return new_message
+def weechat_print(data, modifier, modifier_data, message):
+    message = emoji_to_name_in_string(message)
+    return message
 
 ################################### [ Main ] ###################################
 
@@ -73,4 +122,5 @@ if __name__ == '__main__':
     if weechat.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION,
                         SCRIPT_LICENSE, SCRIPT_DESC, '', ''):
         weechat.hook_modifier('irc_in_privmsg', 'irc_in_privmsg', '')
-        #FIXME: out
+        weechat.hook_modifier('irc_out1_privmsg', 'irc_out1_privmsg', '')
+        weechat.hook_modifier('weechat_print', 'weechat_print', '')
